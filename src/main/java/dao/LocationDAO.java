acc7ef877297d29541e05d7964ad1134ca854eb1
athinat21 <athinatumani4@gmail.com>
2025-08-16 14:24:49 +0200

    Shtimi i metodave tek klasat DAO

--- src/main/java/dao/LocationDAO.java
+++ src/main/java/dao/LocationDAO.java
@@ -1,4 +1,32 @@
 package dao;
 
+import entity.Location;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import util.HibernateUtil;
+
 public class LocationDAO {
+
+    public  void saveLocation(Location location){
+        Session session = HibernateUtil.getSession();
+        Transaction transaction = session.beginTransaction();
+        session.persist(location);
+        transaction.commit();
+    }
+
+    public Location getLocationByCityAndCountry(String city, String country){
+    Session session = HibernateUtil.getSession();
+    Transaction transaction = session.beginTransaction();
+
+    String hql = "FROM Location WHERE city = :city AND country = :country";
+        Query<Location> query = session.createQuery(hql, Location.class);
+        query.setParameter("city",city);
+        query.setParameter("country",country);
+
+        Location location = query.uniqueResult(); //kthen vtm nje location qe perputhet
+        transaction.commit();
+
+        return location;
+    }
 }
--- src/main/java/dao/WeatherDAO.java
+++ src/main/java/dao/WeatherDAO.java
@@ -4,7 +4,10 @@ package dao;
 import entity.WeatherData;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import util.HibernateUtil;
+
+import java.util.List;
 
 //NJE klase dao (data acccess object) sherben per te percaktuar
 // cfare te dhenash duam te dergojme dhe cfare te dhenash presim
@@ -23,5 +26,24 @@ public class WeatherDAO {
                                         // krijon nje rresht te ri ne tabele
 
         transaction.commit();//eshte hap i rendesishem sepse siguron qe ndryshimet e bera jane ruajtur ne databaze
+    }
+
+    //metode qe merr te dhenat e motit per nje qytet dhe vend
+    public List<WeatherData> getWeatherDataByLocation(String city, String country){
+        Session session = HibernateUtil.getSession();
+        Transaction transaction = session.beginTransaction();
+
+        //query per te marr te dhenat e motit per qytetin dhe vendin
+        //qe kemi vendos. Kjo metode ekzekuton nje HQL(Hibernate Query Lnaguage)
+        String hql = "FROM WeatherData wd WHERE wd.location.city = :city AND wd.location.country";
+        //krijo nje query qe eshte e bazuar ne hql dhe ekzekuton ne entitetin WeatherData
+        Query<WeatherData> query = session.createQuery(hql, WeatherData.class);
+        query.setParameter("city", city);
+        query.setParameter("country",country);
+
+        List<WeatherData> weatherDataList = query.list(); //kthimi ne list te dhenave te motit
+        transaction.commit();
+
+        return weatherDataList;
     }
 }
